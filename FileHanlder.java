78f73e04af56c9bcf759f2b9d48e8fe5aa9f4c6b
Adam Carling <adamjcarling@gmail.com>
2024-03-13 16:34:55 -0600

    Changes made

--- FileHanlder.java
+++ FileHanlder.java
@@ -1,27 +1,26 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigInteger;
+import java.util.Formatter;
+import java.util.Scanner;
 public class FileHanlder {
     String _filePath;
-    
-    FileHanlder(String filePath){
-        this._filePath = filePath;
+
+
+public static String scannerIn(String filePath) throws FileNotFoundException {
+    String Num_as_String;
+    File fileToReadFrom = new File(filePath);
+    Scanner myScanner = new Scanner(fileToReadFrom);
+    Num_as_String = myScanner.nextLine();
+    myScanner.close();
+    return Num_as_String;
+}//End Scanner method
+
+public static void writeToFile (String filePath, BigInteger key)throws FileNotFoundException{
+        Formatter formatter = new Formatter(new File(filePath));
+        formatter.format(key.toString());
+        formatter.close();
     }
-    
-    
-    
-    
-    
-    try {
-        FileWriter fileToWriteTo = new FileWriter(_filePath);
-
-        BufferedWriter Bufferedwriter = new BufferedWriter(fileToWriteTo);
-    } catch {
-
-    }
-
-
-
 
 
 
--- Public_Key_Use.java
+++ Public_Key_Use.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 
 public class Public_Key_Use {
     static String _message = "This is the message";
+
     static BigInteger _BigInt_Message = new BigInteger(_message);
     static String _filePath_n = ".txt files/n.txt";
     static String _filePath_e = ".txt files/e.txt";
     static String _FilePath_d = ".txt files/d.txt";
+    static String _filePath_MyMessege = ".txt files/MyMessege.txt";
     static BigInteger n;
     static BigInteger e;
     static BigInteger d;
@@ -18,14 +20,16 @@ public class Public_Key_Use {
 public static void main(String[] args) {
     n = pull_num(_filePath_n);
     e = pull_num(_filePath_e);
-    System.out.println(n);
-    System.out.println();
-    System.out.println(e);
-
-
+        
     _BigInt_Message = Ch31.modularExponentiation(e, _BigInt_Message, n);
     System.out.println(_BigInt_Message);
-}
+    
+    try {
+        FileHanlder.writeToFile(_filePath_MyMessege, e);
+    } catch (Exception e) {
+        // TODO: handle exception
+    }
+}//End Main
 
 public static BigInteger pull_num(String filePath){
     BigInteger x = BigInteger.ZERO;
